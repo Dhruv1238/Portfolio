8868806787d1c331768a07e20b90b43bfa4c699f
Dhruv1238 <dhruv4075@gmail.com>
2024-03-12 14:22:40 +0530

    feat: Vercel Performance insights

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Providers } from "./providers";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Dhruv Sharma | Portfolio",
@@ -10,7 +11,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <SpeedInsights />
+        </Providers>
       </body>
     </html>
   );
